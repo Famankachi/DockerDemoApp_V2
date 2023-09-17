21e861cdf477d7ee093e2bc12f842e962b7e06c0
Famankachi <fahed.amankachi@gmail.com>
2023-09-17 19:45:42 +0100

    Test 1

---
+++ src/main/java/com/Docker/Demo/DockerDemoApp/DockerDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.Docker.Demo.DockerDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Docker/Demo/DockerDemoApp/DockerDemoAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Docker.Demo.DockerDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("TEST 1");
+	}
+
+}
